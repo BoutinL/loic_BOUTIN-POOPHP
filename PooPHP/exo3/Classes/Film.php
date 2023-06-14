f3a89ec4b0f9514d3e122b4a522f14bb11fffade
BoutinL <boutin.loic@hotmail.com>
2023-06-14 14:01:40 +0200

    pause looping

---
+++ PooPHP/exo3/Classes/Film.php
@@ -0,0 +1,11 @@
+<?php
+Class Film{
+
+	public string $_titre;
+	private DateTime $_sortieFr;
+	private int $_dureeMinute;
+	public Realisateur $_realisateur;
+	private string $_synopsis;
+	public string $_genre;
+	public array $_acteurs;
+}
